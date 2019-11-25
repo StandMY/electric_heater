0d45723da2b1e3f8802925f4df097f5453e18c08
Naroro <2319943649@qq.com>
2019-11-25 22:21:10 +0800

    11.25

--- app/src/main/java/com/example/naroro/electric_heater/Activity/LoginActivity.java
+++ app/src/main/java/com/example/naroro/electric_heater/Activity/LoginActivity.java
@@ -1,28 +1,51 @@
 package com.example.naroro.electric_heater.Activity;
 
-import android.app.ActionBar;
 import android.content.Intent;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
-
-import com.example.naroro.electric_heater.Class.BaseActivity;
 import com.example.naroro.electric_heater.R;
 import com.example.naroro.electric_heater.databinding.ActivityLoginBinding;
+
+import java.io.IOException;
+
+import javax.security.auth.login.LoginException;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    private static final int GET =1;
     ActivityLoginBinding mBinding;
-    ActionBar mActionBar;
+    private OkHttpClient client = new OkHttpClient();
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case GET:
+                    //获取数据
+                    Toast.makeText(LoginActivity.this,(String)msg.obj,Toast.LENGTH_LONG).show();
+                    break;
+
+            }
+        }
+    };
+    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +53,6 @@ public class LoginActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_login);
         mBinding = DataBindingUtil.setContentView(this,R.layout.activity_login);
         getSupportActionBar().hide();
-        //mActionBar = getActionBar();
-        //mActionBar.hide();
-
 
 
         mBinding.verficLogin.setOnClickListener(new View.OnClickListener() {
@@ -65,19 +85,51 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String account = mBinding.account.getText().toString();
                 String password = mBinding.password.getText().toString();
-                if(account.equals("2819") && password.equals("123456")){
-                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }else {
-                    Toast.makeText(LoginActivity.this,"Invalid",
-                            Toast.LENGTH_LONG).show();
-                }
-
-
+                getMethodTest();
+//                if(account.equals("2819") && password.equals("123456")){
+//                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//                }else {
+//                    Toast.makeText(LoginActivity.this,"Invalid",
+//                            Toast.LENGTH_LONG).show();
+//                }
             }
         });
 
+    }
+
+    private void getMethodTest() {
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    String result =get("https://raw.githubusercontent.com/square/okhttp/master/README.md");
+                    Log.e(TAG, result);
+                    Message msg = Message.obtain();
+                    msg.what = GET;
+                    msg.obj = result;
+                    mHandler.sendMessage(msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+    }
+
+
+    //get请求
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private String get(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
     }
 
 
--- app/src/main/java/com/example/naroro/electric_heater/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/naroro/electric_heater/Activity/RegisterActivity.java
@@ -1,7 +1,12 @@
 package com.example.naroro.electric_heater.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.naroro.electric_heater.R;
@@ -9,6 +14,13 @@ import com.example.naroro.electric_heater.R;
 public class RegisterActivity extends AppCompatActivity {
 
     private TextView title_text;
+    private EditText user;
+    private EditText tel;
+    private EditText pw;
+    private EditText repeatPw;
+    private EditText address;
+    private Button btn_register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +29,25 @@ public class RegisterActivity extends AppCompatActivity {
 
         title_text = findViewById(R.id.title_textView);
         title_text.setText("欢迎注册瀚清");
+        init();
+
+        //点击注册的响应事件
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
+
+    private void init() {
+        user = findViewById(R.id.user_name);
+        tel = findViewById(R.id.tel);
+        pw = findViewById(R.id.pw);
+        repeatPw = findViewById(R.id.repeatPw);
+        btn_register = findViewById(R.id.btn_register);
+    }
+
 
 }
