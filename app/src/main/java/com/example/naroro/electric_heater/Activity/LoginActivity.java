8931ecc4f2b031184740101ff4d6341c66a11caa
gsynf <18813058359@163.com>
2019-11-26 20:01:18 +0800

    增加登录

--- app/src/main/java/com/example/naroro/electric_heater/Activity/LoginActivity.java
+++ app/src/main/java/com/example/naroro/electric_heater/Activity/LoginActivity.java
@@ -1,59 +1,67 @@
 package com.example.naroro.electric_heater.Activity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
-import android.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.naroro.electric_heater.R;
 import com.example.naroro.electric_heater.databinding.ActivityLoginBinding;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 
-import javax.security.auth.login.LoginException;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
-
 
 public class LoginActivity extends AppCompatActivity {
 
-
-    private static final int GET =1;
     ActivityLoginBinding mBinding;
-    private OkHttpClient client = new OkHttpClient();
+
+    private EditText tel;
+    private EditText pwd;
+    private String pwd1,tel1;
+    private Button login;
+
+
+    final OkHttpClient client = new OkHttpClient();
+
 
     private Handler mHandler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what){
-                case GET:
-                    //获取数据
-                    Toast.makeText(LoginActivity.this,(String)msg.obj,Toast.LENGTH_LONG).show();
-                    break;
-
+            if (msg.what == 1) {
+                String ReturnMessage = (String) msg.obj;
+                Log.i("获取的返回信息", ReturnMessage);
+//                final UserBean userBean = new Gson().fromJson(ReturnMessage, UserBean.class);
+//                final String AA = userBean.getMsg();
+                /***
+                 * 在此处可以通过获取到的Msg值来判断
+                 * 给出用户提示注册成功 与否，以及判断是否用户名已经存在
+                 */
+//                Log.i("MSGhahahha", AA);
             }
         }
     };
-    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login);
         mBinding = DataBindingUtil.setContentView(this,R.layout.activity_login);
         getSupportActionBar().hide();
-
 
         mBinding.verficLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,7 +71,6 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-
         mBinding.register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -79,13 +86,12 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         mBinding.login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String account = mBinding.account.getText().toString();
                 String password = mBinding.password.getText().toString();
-                getMethodTest();
+
 //                if(account.equals("2819") && password.equals("123456")){
 //                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
 //                    startActivity(intent);
@@ -97,40 +103,60 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-    }
+        /**
+         * 初始化
+         */
+        tel = (EditText) findViewById(R.id.account);
+        pwd = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
 
-    private void getMethodTest() {
-        new Thread(){
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tel1 = tel.getText().toString().trim();
+                pwd1 = pwd.getText().toString().trim();
+                postRequest(tel1,pwd1);
+            }
+        });
+
+    }
+    /**
+     * post请求后台
+     * @param telephonenum
+     * @param password
+     */
+    private void postRequest(String telephonenum,String password)  {
+        //建立请求表单，添加上传服务器的参数
+        RequestBody formBody = new FormBody.Builder()
+                .add("telephoneNum",telephonenum)
+                .add("userPassword",password)
+                .build();
+        //发起请求
+        final Request request = new Request.Builder()
+                .url("http://192.168.1.164:8080/tokens/login1?")
+                .post(formBody)
+                .build();
+        //新建一个线程，用于得到服务器响应的参数
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                super.run();
+                Response response = null;
                 try {
-                    String result =get("https://raw.githubusercontent.com/square/okhttp/master/README.md");
-                    Log.e(TAG, result);
-                    Message msg = Message.obtain();
-                    msg.what = GET;
-                    msg.obj = result;
-                    mHandler.sendMessage(msg);
+                    //回调
+                    response = client.newCall(request).execute();
+                    if (response.isSuccessful()) {
+                        //将服务器响应的参数response.body().string())发送到hanlder中，并更新ui
+                        mHandler.obtainMessage(1, response.body().string()).sendToTarget();
+
+                    } else {
+                        mHandler.obtainMessage(1, response.body().string()).sendToTarget();
+                        //throw new IOException("Unexpected code:" + response);
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-        }.start();
-
+        }).start();
     }
-
-
-    //get请求
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private String get(String url) throws IOException {
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
-    }
-
-
 }
